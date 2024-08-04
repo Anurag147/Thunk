86c0153a6e556ebd4d3375a1e8fdee0e3f70b261
Anurag <a.asthana90@gmail.com>
2024-08-04 23:17:31 +0530

    ok

---
+++ src/components/AlbumsList.js
@@ -0,0 +1,9 @@
+import { useFetchAlbumsQuery } from "../store";
+
+function AlbumsList({ user }) {
+  const {data, error, isLoading} = useFetchAlbumsQuery(user);
+  console.log(data);
+  return <div>Albums for {user.name}</div>;
+}
+
+export default AlbumsList;
